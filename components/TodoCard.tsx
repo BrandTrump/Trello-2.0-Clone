1511b4ac5bb80cea0129440e3552ae860f40c897
Brandon Trump <brandontrump101@gmail.com>
2023-06-10 23:06:25 +1200

    feat: implemented draggable todo card component with title of todo & close icon

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
@@ -12,8 +15,31 @@ type Props = {
   dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
 };
 
-function TodoCard({}: Props) {
-  return <div>TodoCard</div>;
+function TodoCard({
+  todo,
+  index,
+  id,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
+}: Props) {
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+
+      {/* Add image here...*/}
+    </div>
+  );
 }
 
 export default TodoCard;
