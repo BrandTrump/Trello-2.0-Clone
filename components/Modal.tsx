6f937b70a20e92fc39a11bcb48b6ea26b6841b6b
Brandon Trump <brandontrump101@gmail.com>
2023-06-11 01:43:49 +1200

    feat: included task type radio group component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,6 +3,7 @@ import { useState, Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useModalStore } from "@/store/ModalStore";
 import { useBoardStore } from "@/store/BoardStore";
+import TaskTypeRadioGroup from "./TaskTypeRadioGroup";
 
 export default function Modal() {
   const [isOpen, closeModal] = useModalStore((state) => [
@@ -58,6 +59,8 @@ export default function Modal() {
                     className="w-full border border-gray-300 rounded-md outline-none p-5"
                   />
                 </div>
+
+                <TaskTypeRadioGroup />
               </Dialog.Panel>
             </Transition.Child>
           </div>
