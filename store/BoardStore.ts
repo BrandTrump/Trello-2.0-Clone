cc9fbc940ba8584cc6c201597e6d70df9c87e131
Brandon Trump <brandontrump101@gmail.com>
2023-06-11 16:40:31 +1200

    feat: addTask function creates new document in DB

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,4 +1,4 @@
-import { databases } from "@/appwrite";
+import { ID, databases } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/helpers/getTodosGroupedByColum";
 import { uploadImage } from "@/helpers/uploadImage";
 import { create } from "zustand";
@@ -60,5 +60,47 @@ export const useBoardStore = create<BoardState>((set) => ({
         };
       }
     }
+
+    const { $id } = await databases.createDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      ID.unique(),
+      {
+        title: todo,
+        status: columnId,
+        // include image if it exists
+        ...(file && { image: JSON.stringify(file) }),
+      }
+    );
+
+    set({ newTaskInput: "" });
+
+    set((state) => {
+      const newColumns = new Map(state.board.columns);
+
+      const newTodo: Todo = {
+        $id,
+        $createdAt: new Date().toISOString(),
+        title: todo,
+        status: columnId,
+        // include image if it exists
+        ...(file && { image: file }),
+      };
+
+      const column = newColumns.get(columnId);
+
+      if (!column) {
+        newColumns.set(columnId, {
+          id: columnId,
+          todos: [newTodo],
+        });
+      } else {
+        newColumns.get(columnId)?.todos.push(newTodo);
+      }
+
+      return {
+        board: { columns: newColumns },
+      };
+    });
   },
 }));
