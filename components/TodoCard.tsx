58c37125d04a3dac22248044551e51440597c9c6
Brandon Trump <brandontrump101@gmail.com>
2023-06-11 17:08:24 +1200

    feat: displays image with todo

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import getUrl from "@/helpers/getUrl";
 import { XCircleIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
@@ -23,6 +26,21 @@ function TodoCard({
   draggableProps,
   dragHandleProps,
 }: Props) {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (todo.image) {
+      const fetchImage = async () => {
+        const url = await getUrl(todo.image!);
+        if (url) {
+          setImageUrl(url.toString());
+        }
+      };
+
+      fetchImage();
+    }
+  }, [todo]);
+
   return (
     <div
       className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -37,7 +55,17 @@ function TodoCard({
         </button>
       </div>
 
-      {/* Add image here...*/}
+      {imageUrl && (
+        <div className="h-full w-full rounded-b-md">
+          <Image
+            src={imageUrl}
+            alt="Task Image"
+            width={400}
+            height={200}
+            className="w-full object-contain rounded-b-md"
+          />
+        </div>
+      )}
     </div>
   );
 }
