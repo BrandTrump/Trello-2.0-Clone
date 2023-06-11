23b57f9e5cad9f3f4dfd38ef4f7dc1b2ea50c8d6
Brandon Trump <brandontrump101@gmail.com>
2023-06-11 15:13:56 +1200

    feat: added image picker to modal & submit button

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,6 +4,8 @@ import { Dialog, Transition } from "@headlessui/react";
 import { useModalStore } from "@/store/ModalStore";
 import { useBoardStore } from "@/store/BoardStore";
 import TaskTypeRadioGroup from "./TaskTypeRadioGroup";
+import Image from "next/image";
+import { PhotoIcon } from "@heroicons/react/24/solid";
 
 export default function Modal() {
   const [isOpen, closeModal] = useModalStore((state) => [
@@ -68,7 +70,29 @@ export default function Modal() {
 
                 <TaskTypeRadioGroup />
 
-                <div>
+                <div className="mt-2">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      imagePickerRef.current?.click();
+                    }}
+                    className="w-full border border-gray-300 rounded-md outline-none p-5 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  >
+                    <PhotoIcon className="h-6 w-6 mr-2 inline-block" />
+                    Upload Image
+                  </button>
+                  {image && (
+                    <Image
+                      src={URL.createObjectURL(image)}
+                      alt="Uploaded Image"
+                      width={200}
+                      height={200}
+                      className="w-full h-44 object-cover mt-2 filter hover:grayscale transition-all duration-150 cursor-not-allowed"
+                      onClick={() => {
+                        setImage(null);
+                      }}
+                    />
+                  )}
                   <input
                     type="file"
                     ref={imagePickerRef}
@@ -78,6 +102,16 @@ export default function Modal() {
                         return setImage(e.target.files![0]);
                     }}
                   />
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="submit"
+                    disabled={!newTaskInput}
+                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-100 disabled:text-gray-300 disabled:cursor-not-allowed"
+                  >
+                    Add Task
+                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
